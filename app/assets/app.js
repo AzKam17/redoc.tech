93da257c8d0cbc8f7e8a07e83a9a3808dbd34aeb
azk <aziz.kamagate@local.lan>
2024-05-07 11:06:31 +0000

    Basic Dashboard

--- app/assets/app.js
+++ app/assets/app.js
@@ -6,5 +6,55 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+//import 'leaflet/dist/leaflet.min.css'
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+import angular from 'angular'
+import L from 'leaflet'
+
+let  app = angular.module('myApp', []);
+app
+    .config(function($interpolateProvider) {
+        $interpolateProvider.startSymbol('[[');
+        $interpolateProvider.endSymbol(']]');
+    })
+    .controller('myCtrl', function($http, $interval,$scope) {
+
+        $scope.data = {
+            count: 0,
+            last: "",
+            data: []
+        };
+
+        // Function to retrieve data from the server
+        function getData() {
+            $http.get('http://localhost:9000/dashboard')
+                .then(function(response) {
+                    $scope.data = response.data;
+
+                    for (let i = 0; i < $scope.data.data.length; i++) {
+                        $scope.data.data[i] = JSON.parse($scope.data.data[i]);
+                    }
+                    console.log('Data Retrieved')
+                })
+                .catch(function(error) {
+                    console.error('Error retrieving data:', error);
+                });
+        }
+
+        // Call getData() immediately
+        getData();
+
+        // Call getData() every minute (60000 milliseconds)
+        $interval(getData, 10 * 1000);
+    });
+
+
+const map = L.map('map', {
+    dragging: false,
+}).setView([5.495492, -4.061254], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    minZoom: 15,
+    attribution: 'CocoaShield Dashbaord v0.0.1'
+}).addTo(map);
+L.marker([5.495492, -4.061254]).addTo(map);
--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\SensorValues;
+use App\Repository\SensorValuesRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,19 +16,64 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return new Response("Ok");
+        return $this->render('index.html.twig');
     }
 
     #[Route('/data', name: 'app_data')]
     public function pidata(Request $request, EntityManagerInterface $manager)
     {
-	$data = $request->getContent();
-	$json = json_decode($data, true);
-	$e = new SensorValues();
-	$e->setData($data);
-	$e->setPiquet($json["node"]);
-	$manager->persist($e);
-	$manager->flush();
-	return new Response('Ok');
+        $data = $request->getContent();
+        $json = json_decode($data, true);
+        $e = new SensorValues();
+        $e->setData($data);
+        $e->setPiquet($json["node"]);
+        $manager->persist($e);
+        $manager->flush();
+        return new Response('Ok');
+    }
+
+    #[Route("/dashboard", name: "dashboard")]
+    public function dashboard(SensorValuesRepository $repository){
+        $count = $repository->count();
+
+        /** @var SensorValues $last */
+        $last = $repository->findOneBy([], ['id' => 'DESC']);
+        $last = $last->getCreatedAt()->format('d/m/y H:i:s');
+
+        $els = $repository->findBy([], ['id' => 'DESC'], 5,0);
+        $e = [];
+        foreach ($els as $s){
+            $t = json_decode($s->getData(), true);
+            $t['date'] = $s->getCreatedAt()->format('d/m/y H:i:s');
+            $e[] = json_encode($t);
+        }
+
+        return new JsonResponse([
+           'count' => $count,
+           'last' => $last,
+           'data' => $e
+        ]);
+    }
+
+
+    #[Route("/export", name: "export")]
+    public function export(SensorValuesRepository $repository){
+        $data = $repository->findAll();
+        $csv = 'Piquet,Humidite,Temperature,Date' . "\n";
+
+        foreach ($data as $el) {
+            $e = json_decode($el->getData(), true);
+            $csv .= $e['node'];
+            $csv .= ',' . $e['hum_air'];
+            $csv .= ',' . $e['temp'];
+            $csv .= ',' . $el->getCreatedAt()->format('d/m/y H:i:s');
+            $csv .= "\n";
+        }
+
+        $response = new Response($csv);
+        $response->headers->set('Content-Type', 'text/csv');
+        $response->headers->set('Content-Disposition', 'attachment; filename="export.csv"');
+
+        return $response;
     }
 }
